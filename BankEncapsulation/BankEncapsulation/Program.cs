fd14bfefd043d7b4aaa1c6b9dd1d658c7b27f85a
AH-SHA <email.sa.coder@gmail.com>
2025-02-23 22:09:51 -0500

    Initial Commit and File Push
    Manually adding files. VSS Git menu and command line not working to push updates to GitHub.

---
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BankEncapsulation
+{
+    public class Program
+    {
+        static void Main(string[]args)
+        {
+            BankAccount userAccount = new BankAccount();
+            
+            Console.WriteLine("Enter a dollar amount to deposit:");
+
+            double userDeposit = double.Parse(Console.ReadLine());
+
+            userAccount.Deposit(userDeposit);
+
+            Console.WriteLine($"Your balance is: $"+ (userAccount.GetBalance()));
+        }
+    }
+}
